b688d98b3432ff6eafa9a4da2dfdbab387c47da4
Ayesh Isuru <ayesh.b@slcdf.org>
2024-02-27 20:21:04 +0530

    Update Middle Content

---
+++ src/components/Counter.jsx
@@ -0,0 +1,13 @@
+import React, { useState, useEffect, useRef } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css'; 
+
+
+const Counters = () => {
+  return (
+    <div>
+
+    </div>
+  );
+};
+
+export default Counters;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Hero from "../components/Hero";
 import Navigation from "../components/Navigation";
 import WhoAreWe from "../components/WhoAreWe";
+import Counters from "../components/Counter";
 
 
 export default function Home() {
@@ -9,6 +10,7 @@ export default function Home() {
     <div>
       <Hero/>
       <WhoAreWe/>
+      <Counters/>
     </div>
   );
 }
