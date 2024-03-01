052e601a58418816e8a59af2e74a2b28d873fa04
Ayesh Isuru <ayesh.b@slcdf.org>
2024-03-01 17:44:00 +0530

    Setup Map

--- src/components/ContactUs/Hero.jsx
+++ src/components/ContactUs/Hero.jsx
@@ -8,7 +8,7 @@ const Hero = () => {
         <div className="row">
             <video autoPlay muted src={contactUs} type="video/mp4"></video>
         </div>
-        <div className="row pt-3" >
+        <div className="row pt-5 pb-5 map-f-div" >
         <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d204305.97107301102!2d174.54131432256708!3d-36.859697088791016!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x6d0d47fb5a9ce6fb%3A0x500ef6143a29917!2sAuckland%2C%20New%20Zealand!5e0!3m2!1sen!2slk!4v1709256700326!5m2!1sen!2slk" width="1920" height="300"  allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
         </div>
     </div>
