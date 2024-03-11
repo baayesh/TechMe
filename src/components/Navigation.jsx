c1a3b41fbad7b2ff977bdbfd4a29ed8cd02621b3
Ayesh Isuru <ayesh.b@slcdf.org>
2024-03-11 22:05:41 +0530

    Change Functions of Nav Bar

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,30 +1,70 @@
 import React from "react";
 import "./styles/Navigation.css";
 import { Link } from "react-router-dom";
+import { useState } from "react";
 
 function Navigation() {
   window.addEventListener("scroll", function () {
     var header = document.querySelector("header");
     header.classList.toggle("scrolled", window.scrollY > 0);
   });
+
+  // Navigation actiovator
+  const [isClassChanged, setIsClassChanged] = useState(false);
+
+  const clickHandler = () => {
+    setIsClassChanged(!isClassChanged);
+  };
+
   return (
-    <header className="container-header" l>
+    <header className="container-header" >
       <div className="  container-fluid d-flex justify-content-center align-items-center">
         <div className="row  middle-header">
-          <ul
-            className="flexnav d-flex justify-content-center align-items-center mt-3"
-            data-breakpoint="768"
-          >
-            <Link className="link ps-2 pe-2 pt-1 pb-1 me-5" to="/">
-              TechMe
-            </Link>
-            <Link className="link ps-2 pe-2 pt-1 pb-1 me-5" to="/web">
-              Services
-            </Link>
-            <Link className="link ps-2 pe-2 pt-1 pb-1" to="/Contacts">
-              Contacts
-            </Link>
-          </ul>
+          <nav>
+            <ul className="side-op-mob">
+              <li onClick={clickHandler}>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  height="24"
+                  viewBox="0 -960 960 960"
+                  width="24"
+                >
+                  <path d="M120-240v-80h720v80H120Zm0-200v-80h720v80H120Zm0-200v-80h720v80H120Z" />
+                </svg>
+              </li>
+            </ul>
+            {/* Side bar for mobile devices */}
+            <ul className={isClassChanged ? 'sidebar-active': 'sidebar'}>
+              <Link className="link ps-2 pe-2 pt-1 pb-1 me-5" to="/">
+                TechMe
+              </Link>
+              <Link className="link ps-2 pe-2 pt-1 pb-1 me-5" to="/web">
+                Services
+              </Link>
+              <Link className="link ps-2 pe-2 pt-1 pb-1 me-5" to="/Contacts">
+                Contacts
+              </Link>
+              <Link className="link ps-2 pe-2 pt-1 pb-1" to="/AboutUs">
+                About Us
+              </Link>
+            </ul>
+
+            {/* Side bar for web browsers */}
+            <ul className="side-web mt-3">
+              <Link className="link ps-2 pe-2 pt-1 pb-1 me-5" to="/">
+                TechMe
+              </Link>
+              <Link className="link ps-2 pe-2 pt-1 pb-1 me-5" to="/web">
+                Services
+              </Link>
+              <Link className="link ps-2 pe-2 pt-1 pb-1" to="/Contacts">
+                Contacts
+              </Link>
+              <Link className="link ps-2 pe-2 pt-1 pb-1" to="/AboutUs">
+                About Us
+              </Link>
+            </ul>
+          </nav>
         </div>
       </div>
     </header>
