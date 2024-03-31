aaf8bcc93739cfd412bc74a11978f7b23a02daa1
Ayesh Bamunuarachchi <ayeshbamunuarachchi@gmail.com>
2024-03-31 18:55:47 +0530

    Add design for landing page

--- src/pages/Home/Hero/Hero.jsx
+++ src/pages/Home/Hero/Hero.jsx
@@ -2,20 +2,49 @@ import React from "react";
 import "../Hero/hero.css";
 import hmmimg from "../../../Assets/Home/hm-mimg-1.png";
 import Button from "../../../components/Button/Button";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 const Hero = () => {
+  const settings = {
+    dots: true,
+    fade: true,
+    autoplay: true,
+    autoplay: true,
+    autoplaySpeed: 2500,
+    speed: 700,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    waitForAnimate: false
+
+  };
   return (
-    <div className="hm-hr-mdiv">
-      <div className="hm-hr-mdivtext">
-        <p className="hm-hr-mtext">
-          Transform your digital presence with expert<span> web development</span>  and<span> social
-          media marketing</span> 
-        </p>
-        <Button 
-        text={"EXPLORE T.ME SERVICES"}/>
-      </div>
-      <div className="hm-hr-mimgdiv">
-            <img className="img-fluid hm-hr-mimg" src={hmmimg} alt="" />
-      </div>
+    <div>
+      <Slider {...settings}>
+        {/* content 1 */}
+        <div className="hm-hr-mdiv hm-hr-background-img-1">
+          <div className="hm-hr-mdivtext">
+            <p className="hm-hr-mtext">
+            Elevate your online presence with development, branding, and maintenance solutions.
+            </p>
+            <Button text={"EXPLORE T.ME SERVICES"} />
+          </div>
+        </div>
+        {/* Content 2 */}
+        <div className="hm-hr-mdiv hm-hr-background-img-2">
+          <div className="hm-hr-mdivtext">
+            <p className="hm-hr-mtext">Comprehensive IT services: support, repairs, upgrades and virus removal solutions.</p>
+            <Button text={"EXPLORE T.ME SERVICES"} />
+          </div>
+        </div>
+        {/* Content 3 */}
+        <div className="hm-hr-mdiv hm-hr-background-img-3">
+          <div className="hm-hr-mdivtext">
+            <p className="hm-hr-mtext">Get in touch for all your digital, branding, and tech needs.</p>
+            <Button text={"Contact Us"} />
+          </div>
+        </div>
+      </Slider>
     </div>
   );
 };
--- src/pages/Home/WhyChooseUs/WhyChooseUs.jsx
+++ src/pages/Home/WhyChooseUs/WhyChooseUs.jsx
@@ -6,32 +6,37 @@ const WhyChooseUs = () => {
   return (
     <div>
       <div className="hm-why-choose-titlediv">
-        <div className="hm-why-choose-titledivtext">
-          <div className="hm-why-choose-titledivtext1">
-            <h3>WE CAN DO</h3>
+
+            <h3>WE CAN DO it together</h3>
           </div>
-          <div className="hm-why-choose-titledivtext2">
-            <h3>IT TOGETHER</h3>
-          </div>
-        </div>
-        <div className="hm-why-choose-titledivimg">
-          <img src={whyChooseUs} alt="whyChooseUs" />
-        </div>
-      </div>
+ 
 
       {/* Start of the content */}
 
       {whyChooseUsCont.map((d) => (
-        <div className="hm-why-choose-content">
-          <div className="hm-why-choose-contenttitlediv">
-            <p>{d.title}</p>
+        <div>
+          <div className="hm-why-choose-content">
+            <div className="hm-why-choose-contenttitlediv">
+              <p>{d.title1}</p>
+            </div>
+            <div className="hm-why-choose-contentdiv">
+              <p>
+              {d.content1}
+              </p>
+            </div>
           </div>
-          <div className="hm-why-choose-contentdiv">
-            <p>
-            {d.content}
-            </p>
+          <div className="hm-why-choose-content">
+            <div className="hm-why-choose-contentdiv2">
+              <p>{d.content2}</p>
+            </div>
+            <div className="hm-why-choose-contenttitlediv2">
+              <p>
+              {d.title2}
+              </p>
+            </div>
           </div>
         </div>
+        
       ))}
     </div>
   );
@@ -39,14 +44,20 @@ const WhyChooseUs = () => {
 
 const whyChooseUsCont = [
   {
-    title: "Why Choose Us 1",
-    content:
+    title1: "Why Choose Us 1",
+    content1:
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+    title2: "Why Choose Us 2",
+    content2:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
   },
   {
-    title: "Why Choose Us 2",
-    content:
+    title1: "Why Choose Us 3",
+    content1:
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+    title2: "Why Choose Us 4",
+    content2:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
   },
   {
     title: "Why Choose Us 3",
@@ -60,18 +71,5 @@ const whyChooseUsCont = [
   },
 ];
 
-const whyChooseUsCont2 = [
-    {
-        title: "Why Choose Us 0",
-        content:
-          "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-      },
-      {
-        title: "Why Choose Us 0.1",
-        content:
-          "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-      },
-    
-]
 
 export default WhyChooseUs;
