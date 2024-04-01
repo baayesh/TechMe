5f66b458a4b2888ea6514ae9d2d458b44039586d
Ayesh Bamunuarachchi <ayeshbamunuarachchi@gmail.com>
2024-04-02 05:29:44 +0530

    Update Image of hero section

--- src/pages/Home/Hero/Hero.jsx
+++ src/pages/Home/Hero/Hero.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import "../Hero/hero.css";
 import hmmimg from "../../../Assets/Home/hm-mimg-1.png";
-import hmmimg2 from "../../../Assets/Home/hm-mimg-2.png"
+import hmmimg2 from "../../../Assets/Home/hm-mimg-2.png";
 import Button from "../../../components/Button/Button";
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
@@ -11,7 +11,6 @@ const Hero = () => {
     dots: true,
     fade: true,
     autoplay: true,
-    autoplay: false,
     autoplaySpeed: 2500,
     speed: 700,
     slidesToShow: 1,
@@ -20,31 +19,32 @@ const Hero = () => {
   };
   return (
     <div>
-      <Slider {...settings}>
-        <div className="hm-hr-mdiv-1">
-          <div className="hm-hr-mdiv-1-text">
-            <p>
-              Elevate your <span>online presence </span>with development,
-              branding, and maintenance <span>solutions.</span>
-            </p>
-            <Button text="Get Started" />
-          </div>
-          <div className="hm-hr-mdiv-1-img">
-            <img src={hmmimg} alt="" />
-          </div>
+    <Slider {...settings}>
+      {/* content 1 */}
+      <div className="hm-hr-mdiv hm-hr-background-img-1">
+        <div className="hm-hr-mdivtext">
+          <p className="hm-hr-mtext">
+          Elevate your <span>online presence</span> with development, branding, and maintenance <span>solutions</span>.
+          </p>
+          <Button text={"EXPLORE T.ME SERVICES"} />
         </div>
-        <div className="hm-hr-mdiv-2">
-          <div className="hm-hr-mdiv-2-img"><img src={hmmimg2} alt="" /></div>
-          <div className="hm-hr-mdiv-2-text">
-            <p>
-              Comprehensive IT services: support, repairs, upgrades and virus
-              removal solutions.
-            </p>
-            <Button text="Get Started" />
-          </div>
+      </div>
+      {/* Content 2 */}
+      <div className="hm-hr-mdiv hm-hr-background-img-2">
+        <div className="hm-hr-mdivtext">
+          <p className="hm-hr-mtext">Comprehensive <span>IT services</span>: support, repairs, upgrades and virus removal <span>solutions.</span></p>
+          <Button text={"EXPLORE T.ME SERVICES"} />
         </div>
-      </Slider>
-    </div>
+      </div>
+      {/* Content 3 */}
+      <div className="hm-hr-mdiv hm-hr-background-img-3">
+        <div className="hm-hr-mdivtext">
+          <p className="hm-hr-mtext"><span>Get in touch </span>for all your digital, branding, and <span>tech needs.</span></p>
+          <Button text={"Contact Us"} />
+        </div>
+      </div>
+    </Slider>
+  </div>
   );
 };
 
