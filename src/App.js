9516dc99729f21733ab9f389711cb72e3066c1bc
Ayesh Bamunuarachchi <ayeshbamunuarachchi@gmail.com>
2024-04-03 21:55:35 +0530

    Design and develop coming soon page

--- src/App.js
+++ src/App.js
@@ -1,51 +1,53 @@
 import "./App.css";
 import Home from "./pages/Home";
 import Navigation from "./components/Navigation/Navigation";
-import { BrowserRouter as Router, Route, Switch, } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Contacts from "./pages/Contacts";
 import Webdevelopment from "./pages/Webdevelopment";
 import AboutUs from "./pages/AboutUs";
 import Footer from "./components/Footer";
-import TestComponent from "./components/TestComponent"; 
-import React from 'react';
+import TestComponent from "./components/TestComponent";
+import React from "react";
 import Hero from "./pages/Home/Hero/Hero";
 import Services from "./pages/Home/Services/Services";
 import WhyChooseUs from "./pages/Home/WhyChooseUs/WhyChooseUs";
 import CustomerReviews from "./pages/Home/Customer Reviews/CustomerReviews";
 import ContactUs from "./pages/Home/Contact Us/ContactUs";
-
- 
+import ComingSoon from "./pages/Coming Soon/ComingSoon";
 
 function App() {
- 
-
   return (
     <div className="App">
       <Router>
-        <Navigation />
-          <Switch>
-            <Route exact path="/">
-              <Hero/>
-              <Services/>
-              <WhyChooseUs/>
-              <ContactUs/>
-              <CustomerReviews/>
-              
-            </Route>
-            <Route path="/contact-us">
-              <Contacts />
-            </Route>
-            <Route path="/services">
-              <Webdevelopment />
-            </Route>
-            <Route path="/about-us">
-              <AboutUs/>
-            </Route>
-            <Route path="/test" >
-              <TestComponent />
-            </Route>
-          </Switch>
-        <Footer />
+        
+        <Switch>
+          <Route exact path="/">
+          <Navigation />
+            <Hero />
+            <Services />
+            <WhyChooseUs />
+            <ContactUs />
+            <CustomerReviews />
+            <Footer />
+          </Route>
+          <Route path="/contact-us">
+            <Contacts />
+          </Route>
+          <Route path="/services">
+            <Webdevelopment />
+          </Route>
+          <Route path="/about-us">
+            <AboutUs />
+          </Route>
+          <Route path="/test">
+            <TestComponent />
+          </Route>
+        </Switch>
+        <Route path="/coming-soon">
+          <ComingSoon />
+        </Route>
+        
+
       </Router>
     </div>
   );
---
+++ src/pages/Coming Soon/ComingSoon.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import comingSoon from '../../Assets/Coming Soon/Coming Soon.png';
+import './ComingSoon.css';
+
+const ComingSoon = () => {
+  return (
+    <div>
+        <div className='csn-mdiv'>
+            <div className='csn-img'>
+                
+            </div>
+            <div className='csn-text-content'>
+                    <p>
+                        Developers are Working!!!
+                        <br />
+                        <div className='line'></div>
+                       
+                        COMING SOON!
+                    </p>
+                    <div className='circle'>
+                       
+                    </div>
+            </div>
+
+        </div>
+    </div>
+  )
+}
+
+export default ComingSoon
