40d9be0a1b79888d26a80a7b4a80eecfd698321e
Ayesh Bamunuarachchi <ayeshbamunuarachchi@gmail.com>
2024-04-06 05:51:35 +0530

    Create responsiveness of coming soon page

--- src/pages/Coming Soon/ComingSoon.jsx
+++ src/pages/Coming Soon/ComingSoon.jsx
@@ -1,31 +1,25 @@
-import React from 'react';
-import comingSoon from '../../Assets/Coming Soon/Coming Soon.png';
-import './ComingSoon.css';
+import React from "react";
+import comingSoon from "../../Assets/Coming Soon/Coming Soon.png";
+import "./ComingSoon.css";
 
 const ComingSoon = () => {
   return (
     <div>
-        <div className='csn-mdiv'>
-            <div className='csn-img'>
-                
-            </div>
-            <div className='csn-text-content'>
-                    <p>
-                        We are working!!!
-                        <br />
-                        <div className='line'></div>
-                       
-
-                       <span>TechMe Developers.</span>
-                    </p>
-                    <div className='circle'>
-                       
-                    </div>
-            </div>
-
+      <div className="csn-mdiv">
+        <div className="csn-img">
+          <div className="circle"></div>
+          <div className="csn-text-content">
+            <p>
+              We are working!!!
+              <br />
+              <div className="line"></div>
+              <span>TechMe Developers.</span>
+            </p>
+          </div>
         </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default ComingSoon
+export default ComingSoon;
