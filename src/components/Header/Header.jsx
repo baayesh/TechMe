a58cccc2335255a42ca8f1a25ee45725a40b700f
Ayesh Bamunuarachchi <ayeshbamunuarachchi@gmail.com>
2024-04-10 05:15:14 +0530

    make responsivenss of the image

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,8 +1,10 @@
 import React from "react";
 import "./Header.css";
+import mig from "../../Assets/Contact us/Main Images/Mig.png";
 
 const Header = () => {
   return <div className="header-mdiv">
+    <img src={mig} alt="" />
     <h2>
         LET'S TALK
     </h2>
