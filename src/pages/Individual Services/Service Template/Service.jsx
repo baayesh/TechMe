51e423ec5585b6670438f71b30fd8ef4a5df810d
Ayesh Bamunuarachchi <ayeshbamunuarachchi@gmail.com>
2024-04-13 07:04:39 +0530

    make responsive service section for individual services

--- src/pages/Individual Services/Service Template/Service.jsx
+++ src/pages/Individual Services/Service Template/Service.jsx
@@ -1,20 +1,35 @@
-import React from 'react';
-import  './Service.css';
+import React from "react";
+import "./Service.css";
+import Button from "../../../components/Button/Button";
 
 const Service = () => {
   return (
-    <div className='indser-main'>
-      <div className='indser-content'>
-        <div className="indser-img">
-
+    <div className="indser-main">
+      <div className="indser-content">
+        <div className="indser-img"></div>
+        <div className="indser-cta">
+          <div className="indser-points">
+            <ul >
+              <li>Point 1</li>
+              <li>Point 2</li>
+              <li>Point 3</li>
+              <li>Point 4</li>
+            </ul>
+            <ul>
+              <li>Point 5</li>
+              <li>Point 6</li>
+              <li>Point 7</li>
+              <li>Point 8</li>
+            </ul>
+          </div>
+          <div className="indser-button">
+          <Button className="indser-button" text={"Contact Us"} />
+          </div>
         </div>
-        <div className="indser-points">
-          
-        </div>
-
+        
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Service
+export default Service;
