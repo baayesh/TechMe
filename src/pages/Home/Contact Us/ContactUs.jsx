668dc77086a28cc00243d432a4e4671814022706
baayesh <ayeshisurukavinda.1998@gmail.com>
2024-05-01 19:17:47 +0530

    Add arrow to the home page contact us area

--- src/pages/Home/Contact Us/ContactUs.jsx
+++ src/pages/Home/Contact Us/ContactUs.jsx
@@ -11,7 +11,36 @@ const ContactUs = () => {
       
     
           <Link to="/contact-us" className="hm-cus-mlink">
-            <p>Contact Us</p>
+            <div className="hm-cus-cus-link">
+              <p>Contact Us</p>
+                <svg
+                  className="hm-hr-m-arrow"
+                    width="50px"
+                    height="50px"
+                    viewBox="0 0 24 24"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                    transform="rotate(90)"
+                  >
+                    <g id="SVGRepo_bgCarrier" stroke-width="0" />
+                    <g
+                      id="SVGRepo_tracerCarrier"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                    />
+                    <g id="SVGRepo_iconCarrier">
+                      {" "}
+                      <path
+                        d="M12 4V20M12 4L8 8M12 4L16 8"
+                        stroke="#000000"
+                        stroke-width="1"
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                      />{" "}
+                    </g>
+                  </svg>
+            </div>
+          
           </Link>
      
        
