e3fdc0a1e596f804b6b76269e2780632954ffcfd
AyeshB <ayeshbamunuarachchi@gmail.com>
2024-05-17 14:36:56 +0530

    Make footer contact details hover effect

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -23,7 +23,7 @@ function Footer() {
           <div>
             <div className="footer-contact-details-quick-links footer-address">
               <div className="footer-column small">
-                <h4>Contact</h4>
+                <h4 className="footer-title-margin">Contact</h4>
                 <div className="">
                   <div className="footer-contact-details">
                     <svg
